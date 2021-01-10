0a882f0e98fe55a7f795e0ffed60826575aa906f
Rubentex <77239189+Rubentex@users.noreply.github.com>
2021-01-10 12:05:31 -0400

    s

--- config.php
+++ config.php
@@ -1,13 +1,12 @@
 <?php
 // Array of the files with an unique ID
 $files = array(
-    'FID12345' => array(
-        'content_type' => 'application/zip', 
-        'suggested_name' => 'tutorials-file.zip', 
-        'file_path' => 'tempfile.zip',
+    'UID12345' => array(
+        'content_type' => 'application/rar', 
+        'suggested_name' => 'codex-file.rar', 
+        'file_path' => 'https://dl.dropboxusercontent.com/s/0nhxpbfek4jvd65/LISTA%20IPTV%20-%20www.m3u8lista.blogspot.com.rar',
         'type' => 'local_file'
-    )
-    
+    ),
 );
 
 // Base URL of the application
@@ -17,12 +16,10 @@ define('BASE_URL','http://'. $_SERVER['HTTP_HOST'].'/');
 define('DOWNLOAD_PATH', BASE_URL.'download-link.php');
 
 // Path of the token directory to store keys
-define('TOKEN_DIR', 'O18sN3ZIj4A');
+define('TOKEN_DIR', '');
 
 // Authentication password to generate download links
 define('OAUTH_PASSWORD','Tutorialswebsite');
 
 // Expiration time of the link (examples: +1 year, +1 month, +5 days, +10 hours)
 define('EXPIRATION_TIME', '+2 minutes');
-// Authentication 
-define('EXPIRATION_CLICKS', '+1000 number');
