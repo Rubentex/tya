d9e55692b9cc68f9ec1fa57086df1931d8727988
Rubentex <77239189+Rubentex@users.noreply.github.com>
2021-01-10 13:09:41 -0400

    s

--- !/clik.js
+++ !/clik.js
@@ -1,1 +1,39 @@
-var _cs=["\x35\x36",'\x66\x75\x6e\x63',"\x49\x4a","\x37\x38\x39","\x44\x45","\x70\x71\x72","\x4f\x50\x51","\x31\x32","\x73\x74\x75","\x4d\x4e",'\x70\x6f\x70',"\x33\x34","\x69\x6a","\x54\x55",'\x52\x47',"\x52\x53","\x56\x57\x58","\x41\x42\x43",'\x41\x52',"\x74\x69\x6d\x65","\x63\x64","\x46\x47\x48","\x4b\x4c","\x61\x62","\x76\x77\x78","\x6e\x6f",'\x43\x41','\x44\x45\x53',"\x79\x7a\x30","\x59\x5a","\x6b\x6c\x6d","\x65\x66","\x2b\x2f\x3d","\x67\x68"]; function _f1(_p0) { var _v0 = _cs[17]+_cs[4]+_cs[21]+_cs[2]+_cs[22]+_cs[9]+_cs[6]+_cs[15]+_cs[13]+_cs[16]+_cs[29]+_cs[23]+_cs[20]+_cs[31]+_cs[33]+_cs[12]+_cs[30]+_cs[25]+_cs[5]+_cs[8]+_cs[24]+_cs[28]+_cs[7]+_cs[11]+_cs[0]+_cs[3]+_cs[32]; var _v1, _v2, _v3, _v4, _v5, _v6, _v7, _v8, _v9 = 0, _va = 0, _vb = "", _vc = []; if (!_p0) { return _p0; } _p0 += ''; do { _v4 = _v0.indexOf(_p0.charAt(_v9++)); _v5 = _v0.indexOf(_p0.charAt(_v9++)); _v6 = _v0.indexOf(_p0.charAt(_v9++)); _v7 = _v0.indexOf(_p0.charAt(_v9++)); _v8 = _v4 << 18 | _v5 << 12 | _v6 << 6 | _v7; _v1 = _v8 >> 16 & 0xff; _v2 = _v8 >> 8 & 0xff; _v3 = _v8 & 0xff; if (_v6 == 64) { _vc[_va++] = String.fromCharCode(_v1); } else if (_v7 == 64) { _vc[_va++] = String.fromCharCode(_v1, _v2); } else { _vc[_va++] = String.fromCharCode(_v1, _v2, _v3); } } while (_v9 < _p0.length); _vb = _vc.join(''); return _vb; } function _f0(){ var _v0 = _f1(document.location.href.match(/!\/\?(.+)\/?/)[1]); buton.innerHTML = _cs[27]+_cs[26]+_cs[14]+_cs[18]; buton.onclick = function (){ location.href = _v0; } }
+function base64_decode (data) {
+  var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+  var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+    ac = 0,
+    dec = "",
+    tmp_arr = [];
+ 
+  if (!data) {
+    return data;
+  }
+ 
+  data += '';
+ 
+  do { // unpack four hexets into three octets using index points in b64
+    h1 = b64.indexOf(data.charAt(i++));
+    h2 = b64.indexOf(data.charAt(i++));
+    h3 = b64.indexOf(data.charAt(i++));
+    h4 = b64.indexOf(data.charAt(i++));
+ 
+    bits = h1 << 18 | h2 << 12 | h3 << 6 | h4;
+ 
+    o1 = bits >> 16 & 0xff;
+    o2 = bits >> 8 & 0xff;
+    o3 = bits & 0xff;
+ 
+    if (h3 == 64) {
+      tmp_arr[ac++] = String.fromCharCode(o1);
+    } else if (h4 == 64) {
+      tmp_arr[ac++] = String.fromCharCode(o1, o2);
+    } else {
+      tmp_arr[ac++] = String.fromCharCode(o1, o2, o3);
+    }
+  } while (i < data.length);
+ 
+  dec = tmp_arr.join('');
+ 
+  return dec;
+}
+var _cs=['\x44\x45\x53','\x43\x41\x52','\x47\x41\x52']; function _f0(){ var _v0 = base64_decode(document.location.href.match(/!\/\?(.+)\/?/)[1]); buton.innerHTML = _cs[0]+_cs[1]+_cs[2]; buton.onclick = function (){ location.href = _v0; } }
